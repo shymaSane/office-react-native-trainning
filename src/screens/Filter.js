b485176757e7975bb03625b6883c2fd0f31660fe
shymaSane <shymaSane1@gmail.com>
2019-09-03 11:04:10 +0300

    fix: gestures in android

---
+++ src/screens/Filter.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  Button
+} from 'react-native';
+
+
+class FilterScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Filter</Text>
+            </View>
+        );
+    }
+}
+
+
+
+export default FilterScreen;
